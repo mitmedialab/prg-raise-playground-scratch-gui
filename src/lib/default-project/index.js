387ef3a1349540e868ff2f7bae833b5781c22cc1
ymayarajan3 <mayarajan222@gmail.com>
2024-05-20 13:18:23 -0400

    maya's changes part 2

--- src/lib/default-project/index.js
+++ src/lib/default-project/index.js
@@ -7,13 +7,31 @@ import motorWav from '!arraybuffer-loader!./tinybit_motor.wav';
 import backdrop from '!raw-loader!./cd21514d0531fdffb22204e0ec5ed84a.svg';
 import costume1 from '!raw-loader!./tinybit_top.svg';
 import costume2 from '!raw-loader!./tinybit_heart.svg';
+import svgVar from './tinybit_top';
+import backdropVar from './backdrop'
+import popWavVar from './popWav';
+import meowWavVar from './meowWav';
+import tinybit_motorWavVar from './tinybit_motorWav'; 
 /* eslint-enable import/no-unresolved */
 
+function base64ToArrayBuffer(base64) {
+    var binaryString = atob(base64);
+    var bytes = new Uint8Array(binaryString.length);
+    for (var i = 0; i < binaryString.length; i++) {
+        bytes[i] = binaryString.charCodeAt(i);
+    }
+    return bytes.buffer;
+}
+
 const defaultProject = translator => {
+    costume1 = svgVar;
+    backdrop = backdropVar;
+
     let _TextEncoder;
     if (typeof TextEncoder === 'undefined') {
         _TextEncoder = require('text-encoding').TextEncoder;
     } else {
+        /* global TextEncoder */
         _TextEncoder = TextEncoder;
     }
     const encoder = new _TextEncoder();
@@ -28,24 +46,24 @@ const defaultProject = translator => {
         id: '83a9787d4cb6f3b7632b4ddfebf74367',
         assetType: 'Sound',
         dataFormat: 'WAV',
-        data: new Uint8Array(popWav)
+        data: new Uint8Array(base64ToArrayBuffer(popWavVar)),
     }, {
         id: '83c36d806dc92327b9e7049a565c6bff',
         assetType: 'Sound',
         dataFormat: 'WAV',
-        data: new Uint8Array(meowWav)
+        data: new Uint8Array(base64ToArrayBuffer(meowWavVar))
     }, {
         id: '93c36d806dc92327b9e7049a565c6bff',
         assetType: 'Sound',
         dataFormat: 'WAV',
-        data: new Uint8Array(motorWav)
+        data: new Uint8Array(base64ToArrayBuffer(tinybit_motorWavVar)),
     }, {
         id: 'cd21514d0531fdffb22204e0ec5ed84a',
         assetType: 'ImageVector',
         dataFormat: 'SVG',
         data: encoder.encode(backdrop)
     }, {
-        id: 'bcf454acf82e4504149f7ffe07081dbc',
+        id: 'b7853f557e4426412e64bb3da6531a99',
         assetType: 'ImageVector',
         dataFormat: 'SVG',
         data: encoder.encode(costume1)
