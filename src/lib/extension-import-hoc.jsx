d4c980b6687c282ff6498bb965bf081d4b7ae715
Maya Rajan <mayarajan222@gmail.com>
2025-03-20 02:10:06 -0400

    it works

--- src/lib/extension-import-hoc.jsx
+++ src/lib/extension-import-hoc.jsx
@@ -6,6 +6,7 @@ import {connect} from 'react-redux';
 import log from './log';
 import sharedMessages from './shared-messages';
 import JSZip from "jszip";
+import makeToolboxXML from './make-toolbox-xml';
 
 import {
     LoadingStates,
@@ -47,6 +48,7 @@ const ExtensionImportHOC = function (WrappedComponent) {
             bindAll(this, [
                 'createFileObjects',
                 'getProjectTitleFromFilename',
+                'handleFinishedLoadingUpload',
                 'handleStartSelectingExtensionUpload',
                 'handleChange',
                 'onload',
@@ -54,9 +56,9 @@ const ExtensionImportHOC = function (WrappedComponent) {
             ]);
         }
         componentDidUpdate (prevProps) {
-            // if (this.props.isLoadingUpload && !prevProps.isLoadingUpload) {
-            //     this.handleFinishedLoadingUpload(); // cue step 5 below
-            // }
+            if (this.props.isLoadingUpload && !prevProps.isLoadingUpload) {
+                this.handleFinishedLoadingUpload(); // cue step 5 below
+            }
         }
         componentWillUnmount () {
             this.removeFileObjects();
@@ -72,18 +74,17 @@ const ExtensionImportHOC = function (WrappedComponent) {
             // still in memory
             this.removeFileObjects();
             // create fileReader
-            this.fileReader1 = new FileReader();
-            this.fileReader1.onload = this.onload;
+            this.fileReader = new FileReader();
+            this.fileReader.onload = this.onload;
             // create <input> element and add it to DOM
-            this.inputElement1 = document.createElement('input');
-            this.inputElement1.accept = '.zip';
-            this.inputElement1.style = 'display: none;';
-            this.inputElement1.type = 'file';
-            this.inputElement1.onchange = this.handleChange; // connects to step 3
-            console.log(this.inputElement1);
-            document.body.appendChild(this.inputElement1);
+            this.inputElement = document.createElement('input');
+            this.inputElement.accept = '.zip';
+            this.inputElement.style = 'display: none;';
+            this.inputElement.type = 'file';
+            this.inputElement.onchange = this.handleChange; // connects to step 3
+            document.body.appendChild(this.inputElement);
             // simulate a click to open file chooser dialog
-            this.inputElement1.click();
+            this.inputElement.click();
         }
 
         readAuxiliaryExtensionInfo(content) {
@@ -109,6 +110,7 @@ const ExtensionImportHOC = function (WrappedComponent) {
           getCommonObject = (id) => window[id];
 
           untilScriptLoaded = (content, { onLoad, onError }) => {
+            console.log("here");
             var scriptTag = document.createElement('script');
             scriptTag.textContent = `${content}`;
             return new Promise((resolve, reject) => {
@@ -128,6 +130,10 @@ const ExtensionImportHOC = function (WrappedComponent) {
             return Promise.all(
             Object.entries(commonMap).map(([id, content]) => {
               if (this.getCommonObject(id)) {
+                if (id == "AuxiliaryExtensionInfo") {
+                    console.log(window["AuxiliaryExtensionInfo"]);
+                    window["AuxiliaryExtensionInfo"][extId] = extJson[extId];
+                }
                 return Promise.resolve(); // Already loaded
               }
               return this.untilScriptLoaded(content, {
@@ -183,12 +189,28 @@ const ExtensionImportHOC = function (WrappedComponent) {
 
                         Promise.all(filePromises).then(() => {
                             this.untilCommonObjects(contentMap);
-                            var scriptTag = document.createElement('script');
-                            scriptTag.textContent = `${JSON.stringify(scriptJs)}`;
-                            document.body.appendChild(scriptTag);
-                            console.log(window["AuxiliaryExtensionInfo"]);
-                            window["AuxiliaryExtensionInfo"][extId] = extJson[extId];
-                            this.props.vm.extensionManager.loadExtensionIdSync(extId);
+                                this.untilScriptLoaded(scriptJs, {
+                                    onLoad: function () {
+                                        
+                                    },
+                                    onError: () => console.log(`Unable to load bundle for ${extId}`)
+                                })
+                                
+                                console.log(window);
+                                console.log(window["appprg95grpinventor"]);
+                                const { Extension, ...aux } = window[extId];
+                                try {
+                                    const extIdClass = class extends Extension {
+                                        constructor(runtime) { super(runtime, ...window["AuxiliaryExtensionInfo"][extId]) }
+                                    }
+                                    const extensionInstance = new extIdClass(this.props.vm.runtime);
+                                    extensionInstance["internal_init"]()
+                                    const serviceName = this.props.vm.extensionManager._registerInternalExtension(extensionInstance);
+                                    this.props.vm.extensionManager._loadedExtensions.set(extId, serviceName);
+                                } catch (e) {
+                                    console.log(e);
+                                }
+
                         });
                         
                         
@@ -198,6 +220,17 @@ const ExtensionImportHOC = function (WrappedComponent) {
                         console.error("Unzip error:", err);
                         this.props.closeFileMenu();
                     });
+                // add script
+                // set auxiliary info
+                // reload toolbox xml
+
+                // if (uploadAllowed) {
+                //     // cues step 4
+                //     this.props.requestProjectUpload(loadingState);
+                // } else {
+                //     // skips ahead to step 7
+                    
+                // }
                 this.removeFileObjects();
                 this.props.closeFileMenu();
             }
@@ -206,7 +239,17 @@ const ExtensionImportHOC = function (WrappedComponent) {
 
         // step 5: called from componentDidUpdate when project state shows
         // that project data has finished "uploading" into the browser
-
+        handleFinishedLoadingUpload () {
+            if (this.fileToUpload && this.fileReader) {
+                // begin to read data from the file. When finished,
+                // cues step 6 using the reader's onload callback
+                this.fileReader.readAsArrayBuffer(this.fileToUpload);
+            } else {
+                this.props.cancelFileUpload(this.props.loadingState);
+                // skip ahead to step 7
+                this.removeFileObjects();
+            }
+        }
         // used in step 6 below
         getProjectTitleFromFilename (fileInputFilename) {
             if (!fileInputFilename) return '';
@@ -219,39 +262,39 @@ const ExtensionImportHOC = function (WrappedComponent) {
         // step 6: attached as a handler on our FileReader object; called when
         // file upload raw data is available in the reader
         onload () {
-            // if (this.fileReader1) {
-            //     this.props.onLoadingStarted();
-            //     const filename = this.fileToUpload && this.fileToUpload.name;
-            //     let loadingSuccess = false;
-            //     this.props.vm.loadProject(this.fileReader1.result)
-            //         .then(() => {
-            //             if (filename) {
-            //                 const uploadedProjectTitle = this.getProjectTitleFromFilename(filename);
-            //                 this.props.onSetProjectTitle(uploadedProjectTitle);
-            //             }
-            //             loadingSuccess = true;
-            //         })
-            //         .catch(error => {
-            //             log.warn(error);
-            //             alert(this.props.intl.formatMessage(messages.loadError)); // eslint-disable-line no-alert
-            //         })
-            //         .then(() => {
-            //             this.props.onLoadingFinished(this.props.loadingState, loadingSuccess);
-            //             // go back to step 7: whether project loading succeeded
-            //             // or failed, reset file objects
-            //             this.removeFileObjects();
-            //         });
-            // }
+            if (this.fileReader) {
+                this.props.onLoadingStarted();
+                const filename = this.fileToUpload && this.fileToUpload.name;
+                let loadingSuccess = false;
+                this.props.vm.loadProject(this.fileReader.result)
+                    .then(() => {
+                        if (filename) {
+                            const uploadedProjectTitle = this.getProjectTitleFromFilename(filename);
+                            this.props.onSetProjectTitle(uploadedProjectTitle);
+                        }
+                        loadingSuccess = true;
+                    })
+                    .catch(error => {
+                        log.warn(error);
+                        alert(this.props.intl.formatMessage(messages.loadError)); // eslint-disable-line no-alert
+                    })
+                    .then(() => {
+                        this.props.onLoadingFinished(this.props.loadingState, loadingSuccess);
+                        // go back to step 7: whether project loading succeeded
+                        // or failed, reset file objects
+                        this.removeFileObjects();
+                    });
+            }
         }
         // step 7: remove the <input> element from the DOM and clear reader and
         // fileToUpload reference, so those objects can be garbage collected
         removeFileObjects () {
-            if (this.inputElement1) {
-                this.inputElement1.value = null;
-                document.body.removeChild(this.inputElement1);
+            if (this.inputElement) {
+                this.inputElement.value = null;
+                document.body.removeChild(this.inputElement);
             }
-            this.inputElement1 = null;
-            this.fileReader1 = null;
+            this.inputElement = null;
+            this.fileReader = null;
             this.fileToUpload = null;
         }
         render () {
@@ -301,35 +344,35 @@ const ExtensionImportHOC = function (WrappedComponent) {
         })
     };
     const mapStateToProps = (state, ownProps) => {
-        // const loadingState = state.scratchGui.projectState.loadingState;
-        // const user = state.session && state.session.session && state.session.session.user;
+        const loadingState = state.scratchGui.projectState.loadingState;
+        const user = state.session && state.session.session && state.session.session.user;
         return {
-            // isLoadingUpload: getIsLoadingUpload(loadingState),
-            // isShowingWithoutId: getIsShowingWithoutId(loadingState),
-            // loadingState: loadingState,
-            // projectChanged: state.scratchGui.projectChanged,
-            // userOwnsProject: ownProps.authorUsername && user &&
-            //     (ownProps.authorUsername === user.username),
+            isLoadingUpload: getIsLoadingUpload(loadingState),
+            isShowingWithoutId: getIsShowingWithoutId(loadingState),
+            loadingState: loadingState,
+            projectChanged: state.scratchGui.projectChanged,
+            userOwnsProject: ownProps.authorUsername && user &&
+                (ownProps.authorUsername === user.username),
             vm: state.scratchGui.vm
         };
     };
     const mapDispatchToProps = (dispatch, ownProps) => ({
-        // cancelFileUpload: loadingState => dispatch(onLoadedProject(loadingState, false, false)),
-        // closeFileMenu: () => dispatch(closeFileMenu()),
-        // // transition project state from loading to regular, and close
-        // // loading screen and file menu
-        // onLoadingFinished: (loadingState, success) => {
-        //     dispatch(onLoadedProject(loadingState, ownProps.canSave, success));
-        //     dispatch(closeLoadingProject());
-        //     dispatch(closeFileMenu());
-        // },
-        // // show project loading screen
-        // onLoadingStarted: () => dispatch(openLoadingProject()),
-        // onSetProjectTitle: title => dispatch(setProjectTitle(title)),
-        // // step 4: transition the project state so we're ready to handle the new
-        // // project data. When this is done, the project state transition will be
-        // // noticed by componentDidUpdate()
-        // requestProjectUpload: loadingState => dispatch(requestProjectUpload(loadingState))
+        cancelFileUpload: loadingState => dispatch(onLoadedProject(loadingState, false, false)),
+        closeFileMenu: () => dispatch(closeFileMenu()),
+        // transition project state from loading to regular, and close
+        // loading screen and file menu
+        onLoadingFinished: (loadingState, success) => {
+            dispatch(onLoadedProject(loadingState, ownProps.canSave, success));
+            dispatch(closeLoadingProject());
+            dispatch(closeFileMenu());
+        },
+        // show project loading screen
+        onLoadingStarted: () => dispatch(openLoadingProject()),
+        onSetProjectTitle: title => dispatch(setProjectTitle(title)),
+        // step 4: transition the project state so we're ready to handle the new
+        // project data. When this is done, the project state transition will be
+        // noticed by componentDidUpdate()
+        requestProjectUpload: loadingState => dispatch(requestProjectUpload(loadingState))
     });
     // Allow incoming props to override redux-provided props. Used to mock in tests.
     const mergeProps = (stateProps, dispatchProps, ownProps) => Object.assign(
