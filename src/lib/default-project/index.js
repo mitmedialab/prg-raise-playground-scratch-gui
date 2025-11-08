9d112aa53d02ea7a496f3ac3624e8e6cf0dfd605
Maya Rajan <mayarajan222@gmail.com>
2025-11-08 18:23:52 -0500

    add costumes

--- src/lib/default-project/index.js
+++ src/lib/default-project/index.js
@@ -7,6 +7,13 @@ import motorWav from '!!arraybuffer-loader!./tinybit_motor.wav';
 import backdrop from '!!raw-loader!./cd21514d0531fdffb22204e0ec5ed84a.svg';
 import costume1 from '!!raw-loader!./tinybit_top.svg';
 import costume2 from '!!raw-loader!./tinybit_heart.svg';
+import goaicostume1 from '!!raw-loader!./goai-neutral1.svg';
+import goaicostume2 from '!!raw-loader!./goai-neutral2.svg';
+import goaicostume3 from '!!raw-loader!./goai-joyful.svg';
+import goaicostume4 from '!!raw-loader!./goai-concerned1.svg';
+import goaicostume5 from '!!raw-loader!./goai-concerned2.svg';
+import goaicostume6 from '!!raw-loader!./goai-sad1.svg';
+import goaicostume7 from '!!raw-loader!./goai-sad2.svg';
 /* eslint-enable import/no-unresolved */
 
 const defaultProject = translator => {
@@ -50,13 +57,43 @@ const defaultProject = translator => {
         id: 'b7853f557e4426412e64bb3da6531a99',
         assetType: 'ImageVector',
         dataFormat: 'SVG',
-        data: encoder.encode(costume1)
+        data: encoder.encode(goaicostume1)
     },
     {
         id: 'e6ddc55a6ddd9cc9d84fe0b4c21e016f',
         assetType: 'ImageVector',
         dataFormat: 'SVG',
-        data: encoder.encode(costume2)
+        data: encoder.encode(goaicostume2)
+    },
+    {
+        id: 'f60f99278455c843b7833fb7615428dd',
+        assetType: 'ImageVector',
+        dataFormat: 'SVG',
+        data: encoder.encode(goaicostume3)
+    },
+    {
+        id: 'b2f75ac1cd84615efaea6a7d7a4ee205',
+        assetType: 'ImageVector',
+        dataFormat: 'SVG',
+        data: encoder.encode(goaicostume4)
+    },
+    {
+        id: '580fba92f23d5592200eb5a9079dc38f',
+        assetType: 'ImageVector',
+        dataFormat: 'SVG',
+        data: encoder.encode(goaicostume5)
+    },
+    {
+        id: 'e51942bb4651e616549cfce1ad36ff83',
+        assetType: 'ImageVector',
+        dataFormat: 'SVG',
+        data: encoder.encode(goaicostume6)
+    },
+    {
+        id: '8313a2229d555bbdb8ce92dffed067ad',
+        assetType: 'ImageVector',
+        dataFormat: 'SVG',
+        data: encoder.encode(goaicostume7)
     }
     ];
 };
